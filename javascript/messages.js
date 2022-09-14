bb9bda1c6acccee2c7644fca88bf86b571b0122e
Peter Saba <petersaba001@gmail.com>
2022-09-14 12:00:40 +0300

    added function to add given data to a div in p tag

--- javascript/messages.js
+++ javascript/messages.js
@@ -8,3 +8,18 @@ async function getData(){
 
     return data
 }
+
+function showData(data){
+    let whereToAdd = document.getElementById("displayMessages")
+    for (let i=0; i<data.length; i++){
+        let newElement = document.createElement("div")
+        
+    }
+}
+
+function addParagToDiv(data, div){
+    let parag = document.createElement("p")
+    let content = document.createTextNode(data)
+    parag.appendChild(content)
+    div.appendChild(parag)
+}
